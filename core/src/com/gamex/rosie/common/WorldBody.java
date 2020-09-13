30cb6eaa01dc96a6ad656bf3f4a8c8583bed8be9
LarsFin <larsfinlay@googlemail.com>
2020-09-13 15:14:36 +0100

    gravity update :: green

--- core/src/com/gamex/rosie/common/WorldBody.java
+++ core/src/com/gamex/rosie/common/WorldBody.java
@@ -1,11 +1,12 @@
 package com.gamex.rosie.common;
 
 import com.badlogic.gdx.math.Vector3;
+import com.gamex.rosie.map.CheckResult;
 import com.gamex.rosie.map.IMap;
 
 public class WorldBody implements IWorldBody {
     private Vector3 worldPosition = Vector3.Zero;
-    private IMap map;
+    private final IMap map;
 
     public WorldBody(IMap map) {
 
@@ -19,7 +20,12 @@ public class WorldBody implements IWorldBody {
 
     public void gravityUpdate() {
 
+        Vector3 gravity = WorldConstants._3dDirections.get(WorldConstants._3dDirection.DOWN);
 
+        while (map.checkEmptyRelative(worldPosition, gravity) == CheckResult.EMPTY) {
+
+            map.putAtRelative(this, gravity);
+        }
     }
 
     public void setWorldPosition(Vector3 worldPosition) {
