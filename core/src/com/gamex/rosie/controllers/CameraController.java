0c1eaf4fb7b3a41a5d022f37bdf784b7092adbd9
Lars C. Finlay <larsfinlay@googlemail.com>
2021-01-11 11:21:22 +0000

    Tests for turning Camera Controller :: Green

--- core/src/com/gamex/rosie/controllers/CameraController.java
+++ core/src/com/gamex/rosie/controllers/CameraController.java
@@ -1,5 +1,7 @@
 package com.gamex.rosie.controllers;
 
+import java.util.HashMap;
+
 import static com.gamex.rosie.common.WorldConstants._3dDirection;
 import static com.gamex.rosie.common.WorldConstants.RotationalDirection;
 
@@ -24,6 +26,30 @@ public class CameraController implements ICameraController {
 
     public void turn(RotationalDirection direction) {
 
-        // TODO
+        if (direction == RotationalDirection.CLOCKWISE) {
+
+            orientation = clockwiseOrientations.get(orientation);
+        } else {
+
+            orientation = antiClockwiseOrientations.get(orientation);
+        }
+    }
+
+    private final static HashMap<_3dDirection, _3dDirection> clockwiseOrientations;
+    private final static HashMap<_3dDirection, _3dDirection> antiClockwiseOrientations;
+
+    static {
+
+        clockwiseOrientations = new HashMap<>();
+        clockwiseOrientations.put(_3dDirection.NORTH, _3dDirection.EAST);
+        clockwiseOrientations.put(_3dDirection.EAST, _3dDirection.SOUTH);
+        clockwiseOrientations.put(_3dDirection.SOUTH, _3dDirection.WEST);
+        clockwiseOrientations.put(_3dDirection.WEST, _3dDirection.NORTH);
+
+        antiClockwiseOrientations = new HashMap<>();
+        antiClockwiseOrientations.put(_3dDirection.NORTH, _3dDirection.WEST);
+        antiClockwiseOrientations.put(_3dDirection.WEST, _3dDirection.SOUTH);
+        antiClockwiseOrientations.put(_3dDirection.SOUTH, _3dDirection.EAST);
+        antiClockwiseOrientations.put(_3dDirection.EAST, _3dDirection.NORTH);
     }
 }
