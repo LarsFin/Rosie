acc89db901f895e690f597ed186c8722dac17f98
Lars C. Finlay <larsfinlay@googlemail.com>
2021-01-12 16:51:15 +0000

    Get Movement as Player :: Green

--- core/src/com/gamex/rosie/controllers/PlayerController.java
+++ core/src/com/gamex/rosie/controllers/PlayerController.java
@@ -2,11 +2,23 @@ package com.gamex.rosie.controllers;
 
 import com.badlogic.gdx.math.Vector3;
 import com.gamex.rosie.common.WorldConstants._3dDirection;
+import com.gamex.rosie.math.Degrees;
+
+import static com.gamex.rosie.common.WorldConstants._3dDirections;
 
 public class PlayerController implements IPlayerController {
 
     public Vector3 getMovement(_3dDirection inputDirection, _3dDirection cameraOrientation) {
 
-        return Vector3.Zero;
+        if (inputDirection == _3dDirection.NONE)
+            return Vector3.Zero;
+
+        float inputDegrees = Degrees.from3dDirection(inputDirection);
+        float cameraDegrees = Degrees.from3dDirection(cameraOrientation);
+
+        float resultantDegrees = Degrees.sumDegrees(inputDegrees, cameraDegrees);
+        _3dDirection resultantDirection = Degrees.as3dDirection(resultantDegrees);
+
+        return _3dDirections.get(resultantDirection);
     }
 }
